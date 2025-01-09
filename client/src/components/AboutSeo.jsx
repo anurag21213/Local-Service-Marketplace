bb56167fae0802601b5b2116449f1e69259b472f
Anurag Kumar <anurag.21213@knit.ac.in>
2025-01-10 00:35:15 +0530

    fixed width issue

--- client/src/components/AboutSeo.jsx
+++ client/src/components/AboutSeo.jsx
@@ -15,7 +15,7 @@ const AboutSeo = () => {
             <div className='p-10 '>
                 <h1 className='text-4xl font-semibold my-5'>Why do we need SEO?</h1>
                 <div className='bg-gradient-to-t from-slate-900 to-black rounded-3xl text-white p-5 flex items-center justify-evenly'>
-                    <ul className='p-5 w-[45%]'>
+                    <ul className='p-5 '>
                         <li className='text-xl border-b-2 border-slate-700 py-5 text-center' >Increase Visibility</li>
                         <li className='text-xl border-b-2 border-slate-700 py-5 text-center' >Build Credibility</li>
                         <li className='text-xl border-b-2 border-slate-700 py-5 text-center' >Increase Visibility</li>
@@ -27,7 +27,7 @@ const AboutSeo = () => {
                         
                     </ul>
                     <div className='bg-white rounded-3xl flex items-center justify-center text-black flex-col p-5'>
-                        <img src={img} alt="img"  />
+                        <img src={img} alt="img" className='w-[300px] h-[300px]'  />
                         <p className='p-10 text-xl'>Search engine optimization is an essential practice for any website looking to improve its visibility and attract more organic traffic. In today's digital age, most users rely on search engines like Google, Bing, or Yahoo to find information, products, and services they need.</p>
                     </div>
                 </div>
